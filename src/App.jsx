a55c234197c7864dd08dcf94ad97ed44f6e6d3e4
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2024-10-30 09:48:34 +0600

    uk

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
+import { useEffect, useState } from "react"
 import { Link, Outlet } from "react-router-dom"
 
 
 
 
 function App() {
+  const [data,setData] =  useState([])
 
 
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_URL)
+       .then(res =>res.json())
+       .then(data => setData(data))
+  },[])
   return (
     <> 
       <ul>
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/signin">sign</Link></li>
       </ul>
+       <ul>
+        {
+          data?.map(item => <li key={item._id}>{item.customerName}</li>)
+        }
+       </ul>
       <Outlet></Outlet>
+
     </>
   )
 }
